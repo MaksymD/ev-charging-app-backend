45ea94ae1e7f3bb24c5919add8edd7674bad47dd
a915284 <maksym.dotsenko@eviden.com>
2025-08-11 20:16:39 +0200

    Initial commit

---
+++ src/test/java/com/evchargingapp/backend/EvChargingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evchargingapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
